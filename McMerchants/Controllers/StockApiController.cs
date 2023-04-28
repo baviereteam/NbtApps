14abed65c26c92d9d2326a23fd5bd1e9ef0aa568
baviereteam <cyrilremy.grageon@gmail.com>
2023-04-29 00:29:14 +0200

    Stock API : now manages alleys

--- McMerchants/Controllers/StockApiController.cs
+++ McMerchants/Controllers/StockApiController.cs
@@ -4,12 +4,14 @@ using McMerchants.Models.Database;
 using McMerchants.Models.Json;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.Extensions.Configuration;
 using NbtTools.Geography;
 using NbtTools.Items;
 using NbtTools.Mca;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text.Json;
 using static System.Formats.Asn1.AsnWriter;
@@ -37,10 +39,12 @@ namespace McMerchants.Controllers
         {
             var results = new StockApiResult();
 
-            IEnumerable<StorageRegion> stores = Context.StorageRegions;
+            IEnumerable<StorageRegion> stores = Context.StorageRegions.Include(s => s.Alleys);
             foreach (var store in stores)
             {
-                results.Stores.Add(store, StoredItemService.FindStoredItems(id, store.Coordinates));
+                results.Stores.Add(
+                    FormatStoreResults(store, StoredItemService.FindStoredItems(id, store.Coordinates))
+                );
             }
 
             IEnumerable<FactoryRegion> factories = Context.FactoryProducts.Where(p => p.Item == id).Select(p => p.Factory);
@@ -51,6 +55,46 @@ namespace McMerchants.Controllers
 
             var json = ResultsToJson(results);
             return json;
+        }
+
+        private StoreStockResult FormatStoreResults(StorageRegion store, IDictionary<Point, int> searchResults)
+        {
+            var storeStock = new StoreStockResult(store);
+
+            foreach (var result in searchResults)
+            {
+                if (result.Value > 0)
+                {
+                    storeStock.Count += result.Value;
+                    try
+                    {
+                        storeStock.AlleysContaining.Add(store.Alleys.First(alley => IsPointInAlley(result.Key, alley)));
+                    }
+                    catch (Exception)
+                    {
+                        // Store has no alleys, or no alley matches this point
+                    }
+                }
+            }
+
+            storeStock.AlleysContaining = storeStock.AlleysContaining.Distinct().ToList();
+            return storeStock;
+        }
+
+        private bool IsPointInAlley(Point p, Alley a)
+        {
+            Console.WriteLine($"Looking for point {p} in alley {a.Name}");
+            if (a.Direction == Alley.AlleyDirection.X && a.Coordinate == p.X && a.LowBoundary <= p.Z && p.Z <= a.HighBoundary)
+            {
+                return true;
+            } 
+
+            if (a.Direction == Alley.AlleyDirection.Z && a.Coordinate == p.Z && a.LowBoundary <= p.X && p.X <= a.HighBoundary)
+            {
+                return true;
+            }
+
+            return false;
         }
 
         private string ResultsToJson(StockApiResult result)
---
+++ McMerchants/Models/Json/StoreStockResult.cs
@@ -0,0 +1,18 @@
+﻿using McMerchants.Models.Database;
+using System.Collections.Generic;
+
+namespace McMerchants.Models.Json
+{
+    public class StoreStockResult
+    {
+        public StorageRegion Store { get; set; }
+        public int Count { get; set; }
+        public ICollection<Alley> AlleysContaining { get; set; }
+
+        public StoreStockResult(StorageRegion store) {
+            Store = store;
+            Count = 0;
+            AlleysContaining = new List<Alley>();
+        }
+    }
+}
