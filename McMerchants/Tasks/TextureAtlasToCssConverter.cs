50933b6ae0690678e9f25e66380b4dd79fba9454
baviereteam <cyrilremy.grageon@gmail.com>
2024-05-12 02:06:10 +0200

    Added a few blocks that did not have a trade icon.

--- McMerchants/Tasks/TextureAtlasToCssConverter.cs
+++ McMerchants/Tasks/TextureAtlasToCssConverter.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Threading;
@@ -17,6 +18,34 @@ namespace McMerchants.Tasks
         private const string WEBROOT_IMG_PATH = "img/atlas.png";
         private const string WEBROOT_CSS_PATH = "css/atlas.css";
         private readonly char[] SPLIT_TABLE = new char[] { '/', '\t' };
+        private readonly Dictionary<string, string> Substitutions = new Dictionary<string, string>()
+        {
+            { "quartz_block_side", "quartz_block" },
+            { "compass_00", "compass" },
+            { "clock_32", "clock" },
+            { "crossbow_standby", "crossbow" },
+            { "pumpkin_side", "pumpkin" },
+            { "melon_side", "melon" },
+
+            // Glass panes use the regular glass texture (because their texture is just a glass "bar", from the side / top)
+            { "glass_pane_top", "glass" },
+            { "black_stained_glass_pane_top",       "black_stained_glass" },
+            { "blue_stained_glass_pane_top",        "blue_stained_glass" },
+            { "brown_stained_glass_pane_top",       "brown_stained_glass" },
+            { "cyan_stained_glass_pane_top",        "cyan_stained_glass" },
+            { "gray_stained_glass_pane_top",        "gray_stained_glass" },
+            { "green_stained_glass_pane_top",       "green_stained_glass" },
+            { "light_blue_stained_glass_pane_top",  "light_blue_stained_glass" },
+            { "light_gray_stained_glass_pane_top",  "light_gray_stained_glass" },
+            { "lime_stained_glass_pane_top",        "lime_stained_glass" },
+            { "magenta_stained_glass_pane_top",     "magenta_stained_glass" },
+            { "orange_stained_glass_pane_top",      "orange_stained_glass" },
+            { "pink_stained_glass_pane_top",        "pink_stained_glass" },
+            { "purple_stained_glass_pane_top",      "purple_stained_glass" },
+            { "red_stained_glass_pane_top",         "red_stained_glass" },
+            { "white_stained_glass_pane_top",       "white_stained_glass" },
+            { "yellow_stained_glass_pane_top",      "yellow_stained_glass" },
+        };
 
         public TextureAtlasToCssConverter(IConfiguration configuration, IWebHostEnvironment environment, ILogger<TextureAtlasToCssConverter> logger) 
         {
@@ -85,8 +114,10 @@ namespace McMerchants.Tasks
                 {
                     case "minecraft:block":
                     case "minecraft:item":
+                        var name = Substitutions.ContainsKey(infos[1]) ? Substitutions[infos[1]] : infos[1];
+
                         sb
-                            .Append($"[data-item='minecraft:{infos[1]}']{{")
+                            .Append($"[data-item='minecraft:{name}']{{")
                             .Append($"background-position: -{infos[2].Substring(2)}px -{infos[3].Substring(2)}px;")
                             .Append("}");
                         break;
