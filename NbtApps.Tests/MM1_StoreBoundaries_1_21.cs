f64ae38aadc117da4d24157d19acfa8a2f150d72
baviereteam <cyrilremy.grageon@gmail.com>
2024-07-28 01:26:42 +0200

    Add 1.21 tests (failing)

---
+++ NbtApps.Tests/MM1_StoreBoundaries_1_21.cs
@@ -0,0 +1,79 @@
+﻿using McMerchants.Database;
+using McMerchants.Models.Database;
+using McMerchantsLib.Stock;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NbtApps.Tests
+{
+    [TestClass]
+    public class MM1_StoreBoundaries_1_21 : TestBase
+    {
+        private const string TEST_DIMENSION = "test_dimension";
+
+        [TestInitialize]
+        public void Setup()
+        {
+            CreateHost(new Dictionary<string, string>()
+            {
+                { TEST_DIMENSION, Path.Combine(FixturesDirectory, "StoreBoundaries-1.21") }
+            });
+
+            var dbContext = Host.Services.GetService<McMerchantsDbContext>();
+
+            // create the test store
+            dbContext.StorageRegions.Add(new StorageRegion()
+            {
+                Name = "Test store",
+                Dimension = TEST_DIMENSION,
+                StartX = -4,
+                StartY = -61,
+                StartZ = 23,
+                EndX = 7,
+                EndY = -56,
+                EndZ = 11
+            });
+            dbContext.SaveChanges();
+        }
+
+        [TestMethod]
+        public void SearchForRedSand()
+        {
+            var StockService = Host.Services.GetService<StockService>();
+            var results = StockService.GetStockOf("minecraft:red_sand");
+
+            Assert.AreEqual(0, results.Factories.Count);
+            Assert.AreEqual(0, results.Trades.Count);
+            Assert.AreEqual(1, results.Stores.Count);
+            Assert.IsNull(results.Stores[0].StockInDefaultAlley);
+            Assert.AreEqual(0, results.Stores[0].StockInOtherAlleys.Count);
+
+            int count = 0;
+            foreach (var bulk in results.Stores[0].StockInBulkContainers)
+            {
+                count += bulk.Value;
+            }
+
+            Assert.AreEqual(7, count);
+        }
+
+        [TestMethod]
+        public void SearchForSlimeBlocks()
+        {
+            var StockService = Host.Services.GetService<StockService>();
+            var results = StockService.GetStockOf("minecraft:slime_block");
+
+            Assert.AreEqual(0, results.Factories.Count);
+            Assert.AreEqual(0, results.Trades.Count);
+            Assert.AreEqual(1, results.Stores.Count);
+            Assert.IsNull(results.Stores[0].StockInDefaultAlley);
+            Assert.AreEqual(0, results.Stores[0].StockInOtherAlleys.Count);
+            Assert.AreEqual(0, results.Stores[0].StockInBulkContainers.Count);
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            CloseConnection();
+        }
+    }
+}
