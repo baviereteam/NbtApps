865bdb94e5b753a534c4864cc7eec4472416f4d3
baviereteam <baviereteam@users.noreply.github.com>
2025-10-27 21:50:57 +0100

    Document database migration process.

--- NbtTools/Database/NbtDbContext.cs
+++ NbtTools/Database/NbtDbContext.cs
@@ -4,11 +4,19 @@ using NbtTools.Items;
 namespace NbtTools.Database
 {
     /**
-     * In order to generate migrations, a terminal must be opened in the McMerchants project.
-     * Use the --project switch to target the NbtTools as migration target:
-     * PS C:\NbtApps\McMerchants> dotnet ef migrations add InitialCreate --project ../NbtTools
-     **/
+      When executing Entity Framework Core commands, you must set the `project`, `startup-project` and `context` parameters so that:
+        * `project` points to the project in which files (migrations, ...) must be read and written,
+        * `context` is the name of the DbContext describing the database to operate on,
+        * `startup-project` is the name of the project containing a reference to `Microsoft.EntityFrameworkCore.Design` package.
 
+        For example:
+        ```
+        dotnet ef database update --project NbtTools --startup-project McMerchantsLib --context NbtDbContext
+        ```
+
+        If the `startup-project` is missing, you will get the following error:
+        > Your startup project 'NbtTools' doesn't reference Microsoft.EntityFrameworkCore.Design. This package is required for the Entity Framework Core Tools to work. Ensure your startup project is correct, install the package, and try again.
+    **/
     public class NbtDbContext : DbContext
     {
         public DbSet<Searchable> Searchables { get; set; }
