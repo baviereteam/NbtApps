90e893ec4066a70e907c5e907f75286cbd293f2a
baviereteam <baviereteam@users.noreply.github.com>
2026-04-12 03:57:30 +0200

    Create the basis for the BOM import feature.

---
+++ McMerchantsLib/Models/Bom/EnrichedBomItem.cs
@@ -0,0 +1,24 @@
+﻿using McMerchantsLib.Models.Database;
+using McMerchantsLib.Stock;
+using NbtTools.Items;
+
+namespace McMerchantsLib.Models.Bom;
+
+public class EnrichedBomItem
+{
+	private BomItem _bomItem;
+	
+	public EnrichedBomItem(BomItem bomItem, Searchable item)
+	{
+		_bomItem = bomItem;
+		Item = item;
+	}
+	
+	public int RequiredQuantity => _bomItem.RequiredQuantity;
+	
+    public Searchable Item { get; set; }
+
+    // Availability
+    public int? WorkzoneQuantity { get; set; } = null;
+    public ItemStockResult? StoredQuantities { get; set; } = null;
+}
