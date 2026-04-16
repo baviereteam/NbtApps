f82082d2d6f8545e3f8fc543f7ed198de1473062
baviereteam <baviereteam@users.noreply.github.com>
2026-04-16 03:08:39 +0200

    Add Discord authentication and authorization

---
+++ McMerchants/Controllers/AuthController.cs
@@ -0,0 +1,207 @@
+﻿using Azure.Core;
+using McMerchants.Models.Discord;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using OpenIddict.Abstractions;
+using OpenIddict.Client.AspNetCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using static OpenIddict.Abstractions.OpenIddictConstants;
+
+namespace McMerchants.Controllers;
+
+[Route("/auth")]
+public class AuthController : Controller
+{
+    private readonly IConfiguration _configuration;
+
+    public AuthController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    [HttpGet("login")]
+    public ActionResult LogIn(string returnUrl)
+    {
+        var properties = new AuthenticationProperties
+        {
+            // Only allow local return URLs to prevent open redirect attacks.
+            RedirectUri = Url.IsLocalUrl(returnUrl) ? returnUrl : "/"
+        };
+
+        // Ask the OpenIddict client middleware to redirect the user agent to the identity provider.
+        return Challenge(properties, OpenIddictClientAspNetCoreDefaults.AuthenticationScheme);
+    }
+
+    [HttpGet("logout")]
+    public async Task<ActionResult> LogOut(string returnUrl)
+    {
+        // Retrieve the identity stored in the local authentication cookie. If it's not available,
+        // this indicate that the user is already logged out locally (or has not logged in yet).
+        //
+        // For scenarios where the default authentication handler configured in the ASP.NET Core
+        // authentication options shouldn't be used, a specific scheme can be specified here.
+        var result = await HttpContext.AuthenticateAsync();
+        if (result is not { Succeeded: true })
+        {
+            // Only allow local return URLs to prevent open redirect attacks.
+            return Redirect(Url.IsLocalUrl(returnUrl) ? returnUrl : "/");
+        }
+
+        // Remove the local authentication cookie before triggering a redirection to the remote server.
+        //
+        // For scenarios where the default sign-out handler configured in the ASP.NET Core
+        // authentication options shouldn't be used, a specific scheme can be specified here.
+        await HttpContext.SignOutAsync();
+
+        var properties = new AuthenticationProperties(new Dictionary<string, string?>
+        {
+            // While not required, the specification encourages sending an id_token_hint
+            // parameter containing an identity token returned by the server for this user.
+            [OpenIddictClientAspNetCoreConstants.Properties.IdentityTokenHint] =
+                result.Properties.GetTokenValue(OpenIddictClientAspNetCoreConstants.Tokens.BackchannelIdentityToken)
+        })
+        {
+            // Only allow local return URLs to prevent open redirect attacks.
+            RedirectUri = Url.IsLocalUrl(returnUrl) ? returnUrl : "/"
+        };
+
+        // Ask the OpenIddict client middleware to redirect the user agent to the identity provider.
+        return SignOut(properties, OpenIddictClientAspNetCoreDefaults.AuthenticationScheme);
+    }
+
+    [HttpGet("login/callback"), HttpPost("login/callback"), IgnoreAntiforgeryToken]
+    public async Task<ActionResult> LogInCallback()
+    {
+        // Retrieve the authorization data validated by OpenIddict as part of the callback handling.
+        var result = await HttpContext.AuthenticateAsync(OpenIddictClientAspNetCoreDefaults.AuthenticationScheme);
+
+        // Important: if the remote server doesn't support OpenID Connect and doesn't expose a userinfo endpoint,
+        // result.Principal.Identity will represent an unauthenticated identity and won't contain any user claim.
+        //
+        // Such identities cannot be used as-is to build an authentication cookie in ASP.NET Core (as the
+        // antiforgery stack requires at least a name claim to bind CSRF cookies to the user's identity) but
+        // the access/refresh tokens can be retrieved using result.Properties.GetTokens() to make API calls.
+        if (result is not { Succeeded: true, Principal.Identity.IsAuthenticated: true })
+        {
+            throw new InvalidOperationException("The external authorization data cannot be used for authentication.");
+        }
+
+        // Build an identity based on the external claims and that will be used to create the authentication cookie.
+        var identity = new ClaimsIdentity(
+            authenticationType: "ExternalLogin",
+            nameType: ClaimTypes.Name,
+            roleType: ClaimTypes.Role);
+
+        // By default, OpenIddict will automatically try to map the email/name and name identifier claims from
+        // their standard OpenID Connect or provider-specific equivalent, if available. If needed, additional
+        // claims can be resolved from the external identity and copied to the final authentication cookie.
+        identity.SetClaim(ClaimTypes.Name, result.Principal.GetClaim(ClaimTypes.Name))
+                .SetClaim(ClaimTypes.NameIdentifier, result.Principal.GetClaim(ClaimTypes.NameIdentifier));
+
+        // Preserve the registration details to be able to resolve them later.
+        identity.SetClaim(Claims.Private.RegistrationId, result.Principal.GetClaim(Claims.Private.RegistrationId))
+                .SetClaim(Claims.Private.ProviderName, result.Principal.GetClaim(Claims.Private.ProviderName));
+
+        // We'll use this property to authorize the user through the adequate pages
+        if (await IsUserInGuild(result.Properties))
+        {
+            identity.SetClaim("IsInGuild", true);
+        }
+
+        // Important: when using ASP.NET Core Identity and its default UI, the identity created in this action is
+        // not directly persisted in the final authentication cookie (called "application cookie" by Identity) but
+        // in an intermediate authentication cookie called "external cookie" (the final authentication cookie is
+        // later created by Identity's ExternalLogin Razor Page by calling SignInManager.ExternalLoginSignInAsync()).
+        //
+        // Unfortunately, this process doesn't preserve the claims added here, which prevents flowing claims
+        // returned by the external provider down to the final authentication cookie. For scenarios that
+        // require that, the claims can be stored in Identity's database by calling UserManager.AddClaimAsync()
+        // directly in this action or by scaffolding the ExternalLogin.cshtml page that is part of the default UI:
+        // https://learn.microsoft.com/en-us/aspnet/core/security/authentication/social/additional-claims#add-and-update-user-claims.
+        //
+        // Alternatively, if flowing the claims from the "external cookie" to the "application cookie" is preferred,
+        // the default ExternalLogin.cshtml page provided by Identity can be scaffolded to replace the call to
+        // SignInManager.ExternalLoginSignInAsync() by a manual sign-in operation that will preserve the claims.
+        // For scenarios where scaffolding the ExternalLogin.cshtml page is not convenient, a custom SignInManager
+        // with an overridden SignInOrTwoFactorAsync() method can also be used to tweak the default Identity logic.
+        //
+        // For more information, see https://haacked.com/archive/2019/07/16/external-claims/ and
+        // https://stackoverflow.com/questions/42660568/asp-net-core-identity-extract-and-save-external-login-tokens-and-add-claims-to-l/42670559#42670559.
+
+        // Build the authentication properties based on the properties that were added when the challenge was triggered.
+        var properties = new AuthenticationProperties(result.Properties.Items)
+        {
+            RedirectUri = result.Properties.RedirectUri ?? "/",
+
+            // Set the creation and expiration dates of the ticket to null to decorrelate the lifetime
+            // of the resulting authentication cookie from the lifetime of the identity token returned by
+            // the authorization server (if applicable). In this case, the expiration date time will be
+            // automatically computed by the cookie handler using the lifetime configured in the options.
+            //
+            // Applications that prefer binding the lifetime of the ticket stored in the authentication cookie
+            // to the identity token returned by the identity provider can remove or comment these two lines:
+            IssuedUtc = null,
+            ExpiresUtc = null,
+
+            // Note: this flag controls whether the authentication cookie that will be returned to the
+            // browser will be treated as a session cookie (i.e destroyed when the browser is closed)
+            // or as a persistent cookie. In both cases, the lifetime of the authentication ticket is
+            // always stored as protected data, preventing malicious users from trying to use an
+            // authentication cookie beyond the lifetime of the authentication ticket itself.
+            IsPersistent = false
+        };
+
+        // If needed, the tokens returned by the authorization server can be stored in the authentication cookie.
+        //
+        // To make cookies less heavy, tokens that are not used are filtered out before creating the cookie.
+        properties.StoreTokens(result.Properties.GetTokens().Where(token => token.Name is
+            // Preserve the access, identity and refresh tokens returned in the token response, if available.
+            OpenIddictClientAspNetCoreConstants.Tokens.BackchannelAccessToken or
+            OpenIddictClientAspNetCoreConstants.Tokens.BackchannelIdentityToken or
+            OpenIddictClientAspNetCoreConstants.Tokens.RefreshToken));
+
+        // Ask the default sign-in handler to return a new cookie and redirect the
+        // user agent to the return URL stored in the authentication properties.
+        //
+        // For scenarios where the default sign-in handler configured in the ASP.NET Core
+        // authentication options shouldn't be used, a specific scheme can be specified here.
+        return SignIn(new ClaimsPrincipal(identity), properties);
+    }
+
+    [HttpGet("logout/callback"), HttpPost("logout/callback"), IgnoreAntiforgeryToken]
+    public async Task<ActionResult> LogOutCallback()
+    {
+        // Retrieve the data stored by OpenIddict in the state token created when the logout was triggered.
+        var result = await HttpContext.AuthenticateAsync(OpenIddictClientAspNetCoreDefaults.AuthenticationScheme);
+
+        // In this sample, the local authentication cookie is always removed before the user agent is redirected
+        // to the authorization server. Applications that prefer delaying the removal of the local cookie can
+        // remove the corresponding code from the logout action and remove the authentication cookie in this action.
+
+        return Redirect(result?.Properties?.RedirectUri ?? "/");
+    }
+
+    private async Task<bool> IsUserInGuild(AuthenticationProperties properties) 
+    {
+        var token = properties.GetTokenValue(OpenIddictClientAspNetCoreConstants.Tokens.BackchannelAccessToken);
+
+        using (var client = new HttpClient()
+        {
+            BaseAddress = new Uri(_configuration["DiscordOAuth:discordApiBaseUrl"])
+        })
+        {
+            client.DefaultRequestHeaders.Add(HttpHeader.Names.Authorization, $"Bearer {token}");
+            var request = await client.GetAsync("users/@me/guilds");
+            var userGuildsList = await request.Content.ReadFromJsonAsync<Guild[]>();
+            return userGuildsList.Any(guild => guild.id == _configuration["DiscordOAuth:guildId"]);
+        }
+    }
+}
