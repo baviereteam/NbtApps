adb1746a8b7e64e2081395affd9f929d5fc07478
baviereteam <baviereteam@users.noreply.github.com>
2026-04-17 02:13:12 +0200

    Login button / user avatar

--- McMerchants/Controllers/AuthController.cs
+++ McMerchants/Controllers/AuthController.cs
@@ -40,43 +40,6 @@ public class AuthController : Controller
         return Challenge(properties, OpenIddictClientAspNetCoreDefaults.AuthenticationScheme);
     }
 
-    [HttpGet("logout")]
-    public async Task<ActionResult> LogOut(string returnUrl)
-    {
-        // Retrieve the identity stored in the local authentication cookie. If it's not available,
-        // this indicate that the user is already logged out locally (or has not logged in yet).
-        //
-        // For scenarios where the default authentication handler configured in the ASP.NET Core
-        // authentication options shouldn't be used, a specific scheme can be specified here.
-        var result = await HttpContext.AuthenticateAsync();
-        if (result is not { Succeeded: true })
-        {
-            // Only allow local return URLs to prevent open redirect attacks.
-            return Redirect(Url.IsLocalUrl(returnUrl) ? returnUrl : "/");
-        }
-
-        // Remove the local authentication cookie before triggering a redirection to the remote server.
-        //
-        // For scenarios where the default sign-out handler configured in the ASP.NET Core
-        // authentication options shouldn't be used, a specific scheme can be specified here.
-        await HttpContext.SignOutAsync();
-
-        var properties = new AuthenticationProperties(new Dictionary<string, string?>
-        {
-            // While not required, the specification encourages sending an id_token_hint
-            // parameter containing an identity token returned by the server for this user.
-            [OpenIddictClientAspNetCoreConstants.Properties.IdentityTokenHint] =
-                result.Properties.GetTokenValue(OpenIddictClientAspNetCoreConstants.Tokens.BackchannelIdentityToken)
-        })
-        {
-            // Only allow local return URLs to prevent open redirect attacks.
-            RedirectUri = Url.IsLocalUrl(returnUrl) ? returnUrl : "/"
-        };
-
-        // Ask the OpenIddict client middleware to redirect the user agent to the identity provider.
-        return SignOut(properties, OpenIddictClientAspNetCoreDefaults.AuthenticationScheme);
-    }
-
     [HttpGet("login/callback"), HttpPost("login/callback"), IgnoreAntiforgeryToken]
     public async Task<ActionResult> LogInCallback()
     {
@@ -109,6 +72,14 @@ public class AuthController : Controller
         // Preserve the registration details to be able to resolve them later.
         identity.SetClaim(Claims.Private.RegistrationId, result.Principal.GetClaim(Claims.Private.RegistrationId))
                 .SetClaim(Claims.Private.ProviderName, result.Principal.GetClaim(Claims.Private.ProviderName));
+
+        if (result.Principal.HasClaim("avatar"))
+        {
+            identity.SetClaim(
+                "avatar",
+                $"https://cdn.discordapp.com/avatars/{result.Principal.GetClaim("id")}/{result.Principal.GetClaim("avatar")}.png?size=48"
+            );
+        }
 
         // We'll use this property to authorize the user through the adequate pages
         if (await IsUserInGuild(result.Properties))
@@ -174,19 +145,6 @@ public class AuthController : Controller
         // For scenarios where the default sign-in handler configured in the ASP.NET Core
         // authentication options shouldn't be used, a specific scheme can be specified here.
         return SignIn(new ClaimsPrincipal(identity), properties);
-    }
-
-    [HttpGet("logout/callback"), HttpPost("logout/callback"), IgnoreAntiforgeryToken]
-    public async Task<ActionResult> LogOutCallback()
-    {
-        // Retrieve the data stored by OpenIddict in the state token created when the logout was triggered.
-        var result = await HttpContext.AuthenticateAsync(OpenIddictClientAspNetCoreDefaults.AuthenticationScheme);
-
-        // In this sample, the local authentication cookie is always removed before the user agent is redirected
-        // to the authorization server. Applications that prefer delaying the removal of the local cookie can
-        // remove the corresponding code from the logout action and remove the authentication cookie in this action.
-
-        return Redirect(result?.Properties?.RedirectUri ?? "/");
     }
 
     private async Task<bool> IsUserInGuild(AuthenticationProperties properties) 
